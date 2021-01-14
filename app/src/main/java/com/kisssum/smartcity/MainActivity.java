39c4de418144d50dd717b693b1b17fd56802d3b7
sea <1284700369@qq.com>
2021-01-14 11:20:33 +0800

    适配通知栏

--- app/src/main/java/com/kisssum/smartcity/MainActivity.java
+++ app/src/main/java/com/kisssum/smartcity/MainActivity.java
@@ -2,7 +2,9 @@ package com.kisssum.smartcity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +14,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         getSupportActionBar().hide();
+        getWindow().setStatusBarColor(Color.TRANSPARENT);
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
     }
 }
